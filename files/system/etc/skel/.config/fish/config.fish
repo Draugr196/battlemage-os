dfe18f76b2e01c139a18c70649ef335d69e43300
Draugr196 <jack.doolittle@protonmail.com>
2026-08-29 13:28:01 -0400

    changed fish theme

--- files/system/etc/skel/.config/fish/config.fish
+++ files/system/etc/skel/.config/fish/config.fish
@@ -1,11 +1,11 @@
 if status is-interactive
-    fish_config theme choose Solarized\ Light
 
     alias ls="eza -x --icons=always"
     alias la="eza -xa --icons=always"
     alias ll="eza -la --icons=always"
     alias lt="eza -TRa --icons=always"
     alias cd="z"
+    alias battlemage-update="rpm-ostree upgrade"
 
     set -gx FZF_DEFAULT_OPTS "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
     set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head --200'"
